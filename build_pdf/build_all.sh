141841db3b46ea9e13d7dfadcb035695aa4bed89
Antoine Le Morvan <Antoine Le Morvan>
2023-05-22 15:05:52 +0200

    feat: modular improvments

--- build_pdf/build_all.sh
+++ build_pdf/build_all.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
-config_files=( 
-  build_admin_book
-  build_ansible_book
-  build_bash_book 
-  build_admin_book_it 
-  build_ansible_book_it
-  build_bash_book_it
-  build_ansible_book_es
-)
+# config_files=( 
+#   admin_book/en
+#   ansible_book/build_ansible_book
+#   admin_book/build_admin_book_it
+#   bash_book/build_bash_book 
+   
+#   ansible_book/build_ansible_book_it
+#   bash_book/build_bash_book_it
+#   ansible_book/build_ansible_book_es
+# )
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+cd $SCRIPTPATH
 
-for config_file in "${config_files[@]}"
+for book in admin_book ansible_book bash_book
 do
-  echo "Building $config_file"
-  echo "---------------------"
-  mkdocs build -f $config_file.yml
-  echo "" 
+  for config_file in $(ls ${SCRIPTPATH}/${book}/*.yml)
+  do
+    echo "Building $config_file"
+    echo "---------------------"
+    mkdocs build -f $config_file
+    echo "" 
+  done
+  rm -Rf ${SCRIPTPATH}/${book}/site/
 done
