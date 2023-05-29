e71658253c7ae4f1acfee2fb1d02ae878576f6a2
Antoine Le Morvan <antoine@le-morvan.com>
2023-05-29 10:47:43 +0200

    feat: new chapters for admin book and new book with all middlewares guide

--- build_pdf/build_all.sh
+++ build_pdf/build_all.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 
-# config_files=( 
-#   admin_book/en
-#   ansible_book/build_ansible_book
-#   admin_book/build_admin_book_it
-#   bash_book/build_bash_book 
-   
-#   ansible_book/build_ansible_book_it
-#   bash_book/build_bash_book_it
-#   ansible_book/build_ansible_book_es
-# )
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd $SCRIPTPATH
 
-for book in admin_book ansible_book bash_book
+for book in admin_book ansible_book bash_book middlewares
 do
   for config_file in $(ls ${SCRIPTPATH}/${book}/*.yml)
   do
