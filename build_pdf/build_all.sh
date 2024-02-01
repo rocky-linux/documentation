5f0da93a94cbcbf85316e5cb81feaa6d720b4bc9
Antoine Le Morvan <antoine@le-morvan.com>
2024-02-01 13:25:09 +0100

    fix: missing directory

--- build_pdf/build_all.sh
+++ build_pdf/build_all.sh
@@ -3,7 +3,7 @@
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd $SCRIPTPATH
 
-for book in admin_book ansible_book bash_book middlewares
+for book in admin_book ansible_book bash_book middlewares sed_awk_grep
 do
   for config_file in $(ls ${SCRIPTPATH}/${book}/*.yml)
   do
