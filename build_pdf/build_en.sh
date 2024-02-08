68ea2b61a6c78f3fece2d2593a56f2123f0fe2fd
Antoine Le Morvan <antoine@le-morvan.com>
2024-02-08 10:10:16 +0100

    Add a script that build only source (EN) books

---
+++ build_pdf/build_en.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+cd $SCRIPTPATH
+
+for book in admin_book ansible_book bash_book middlewares sed_awk_grep nvchad_book
+
+do
+  for config_file in $(ls ${SCRIPTPATH}/${book}/en.yml)
+  do
+    echo "Building $config_file"
+    echo "---------------------"
+    VERSION=$(date +%Y/%m/%d) mkdocs build -q -f $config_file -d ${SCRIPTPATH}/site/
+    echo "" 
+  done
+done
+rm -Rf ${SCRIPTPATH}/site/
